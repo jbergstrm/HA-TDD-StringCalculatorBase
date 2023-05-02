2b51c70fffffa0aa23d503af9e72b7609f0ba4d1
Joakim <joakim.bergstrom@live.fi>
2023-05-02 11:08:51 +0300

    code refactored to support test testWithUserSpecifiedDelimiter

--- src/main/java/StringCalculatorImpl.java
+++ src/main/java/StringCalculatorImpl.java
@@ -2,11 +2,21 @@ import java.util.Arrays;
 
 public class StringCalculatorImpl implements StringCalculator {
 
-    private final static String DELIMITER_REGEX = "[,\n]";
+    private final static String USER_SPECIFIED_DELIMITER = "//";
+    private String DELIMITER_REGEX = "[,\n]";
 
     @Override
     public int add(String input) {
-        return input.isEmpty() ? 0 : Arrays
+        if (input.isEmpty()) {
+            return 0;
+        }
+
+        if (input.startsWith(USER_SPECIFIED_DELIMITER)) {
+            DELIMITER_REGEX = "[" + input.charAt(2) + "]";
+            input = input.substring(input.indexOf('\n') + 1);
+        }
+
+        return Arrays
                 .stream(input.split(DELIMITER_REGEX))
                 .mapToInt(Integer::parseInt)
                 .sum();
