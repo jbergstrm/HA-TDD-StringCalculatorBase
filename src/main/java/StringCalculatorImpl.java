9c1df0be88a68bff4ef9cdf33c174fe131279607
Joakim <joakim.bergstrom@live.fi>
2023-05-02 10:41:39 +0300

    code refactored to support test testWithTwoNumbersSeparatedByComma

--- src/main/java/StringCalculatorImpl.java
+++ src/main/java/StringCalculatorImpl.java
@@ -1,7 +1,12 @@
+import java.util.Arrays;
+
 public class StringCalculatorImpl implements StringCalculator {
 
     @Override
     public int add(String input) {
-        return input.isEmpty() ? 0 : Integer.parseInt(input);
+        return input.isEmpty() ? 0 : Arrays
+                .stream(input.split(","))
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 }
