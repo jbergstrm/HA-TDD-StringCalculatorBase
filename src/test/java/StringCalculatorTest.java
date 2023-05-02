230cfdfc15b7936bc32386b03a83976395aba631
Joakim <joakim.bergstrom@live.fi>
2023-05-02 10:37:07 +0300

    test written to assert input with two numbers separated by comma

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -22,4 +22,11 @@ public class StringCalculatorTest {
         Assertions.assertEquals(7, calculator.add("7"));
         Assertions.assertEquals(11, calculator.add("11"));
     }
+
+    @Test
+    public void testWithTwoNumbersSeparatedByComma() {
+        Assertions.assertEquals(3, calculator.add("1,2"));
+        Assertions.assertEquals(13, calculator.add("4,9"));
+        Assertions.assertEquals(16, calculator.add("11,5"));
+    }
 }
