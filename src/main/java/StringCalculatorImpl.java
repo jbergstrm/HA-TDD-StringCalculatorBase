95a6106ed0f84389071d6fea24d61e73b12642f9
Joakim <joakim.bergstrom@live.fi>
2023-05-02 10:58:12 +0300

    code refactored to support tests testWithTwoNumbersSeparatedByNewline and testWithUnspecifiedAmountOfNumbersSeparatedByCommaOrNewline

--- src/main/java/StringCalculatorImpl.java
+++ src/main/java/StringCalculatorImpl.java
@@ -2,10 +2,12 @@ import java.util.Arrays;
 
 public class StringCalculatorImpl implements StringCalculator {
 
+    private final static String DELIMITER_REGEX = "[,\n]";
+
     @Override
     public int add(String input) {
         return input.isEmpty() ? 0 : Arrays
-                .stream(input.split(","))
+                .stream(input.split(DELIMITER_REGEX))
                 .mapToInt(Integer::parseInt)
                 .sum();
     }
